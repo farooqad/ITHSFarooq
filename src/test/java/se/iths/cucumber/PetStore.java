b8d08e21fe7c086a2e3a56f4a8384cfa74188b5f
farooqad <34812327+farooqad@users.noreply.github.com>
2019-05-22 19:12:15 +0200

    Cucumber

---
+++ src/test/java/se/iths/cucumber/PetStore.java
@@ -0,0 +1,46 @@
+package se.iths.cucumber;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import se.iths.petstore.Pet;
+import se.iths.petstore.PetStoreClient;
+
+import java.util.Collections;
+import java.util.List;
+
+public class PetStore {
+
+    @Given("^a pet with id (\\d+)$")
+    public void a_pet_with_id(Integer petId) throws Throwable {
+
+        Pet myPet = new Pet(
+                petId,
+                "Kjell",
+                Collections.emptyList(),
+                "available"
+        );
+
+        new PetStoreClient().createPet(myPet);
+    }
+
+    @When("^I delete the pet with id (\\d+)$")
+    public void i_delete_the_pet_with_id(int petId) throws Throwable {
+
+       /* new PetStoreClient().deletePet(petId);
+
+        List<Pet> myPets = new PetStoreClient().getPetsByStatus("pending");
+
+        CucumberHooks
+                .getWorld()
+                .setPetresponseList(myPets);
+*/
+    }
+
+    @Then("^I get an error fetching the pet with id (\\d+)$")
+    public void i_get_an_error_fetching_the_pet_with_id(int arg1) throws Throwable {
+        new PetStoreClient().getPet(arg1, 404);
+
+        System.out.println(CucumberHooks.getWorld().petresponseList.get(0));
+    }
+}
---
+++ src/test/java/se/iths/cucumber/momondo.java
@@ -0,0 +1,30 @@
+package se.iths.cucumber;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import org.openqa.selenium.WebDriver;
+
+public class momondo {
+    WebDriver myBrowser;
+    @Given("New User with a unique email Id should be registered")
+    public void new_User_with_a_unique_email_Id_should_be_registered() {
+
+
+        throw new cucumber.api.PendingException();
+    }
+
+    @When("Only username and password")
+    public void only_username_and_password() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @Then("Login Successful")
+    public void login_Successful() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+
+}
