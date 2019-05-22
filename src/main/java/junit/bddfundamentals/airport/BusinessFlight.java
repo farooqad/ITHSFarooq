f865d6f6c4762377f1da19f1a30272839bdd0acd
farooqad <34812327+farooqad@users.noreply.github.com>
2019-05-22 19:23:42 +0200

    practice

---
+++ src/main/java/junit/bddfundamentals/airport/BusinessFlight.java
@@ -0,0 +1,22 @@
+package junit.bddfundamentals.airport;
+
+public class BusinessFlight extends Flight {
+	
+	public BusinessFlight(String id) {
+		super(id);
+	}
+
+	@Override
+	public boolean addPassenger(Passenger passenger) {
+		if (passenger.isVip()) {
+			return passengersSet.add(passenger);
+		}
+		return false;
+	}
+
+	@Override
+	public boolean removePassenger(Passenger passenger) {
+		return false;
+	}
+
+}
